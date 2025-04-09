a63663ab99b3712b6bf1b28389a771c89449d5cc
ImNotMeAnymore <172158294+ImNotMeAnymore@users.noreply.github.com>
2025-04-09 12:38:57 -0400

    Jumper's going to be like that jumping dinosaur minigame game

--- nengin/examples/jumper.py
+++ nengin/examples/jumper.py
@@ -14,7 +14,7 @@ def loadText(text:str, f:font.Font) -> Texture:
 
 @addScene("end", windowSize=SIZE)
 class GameOver(Scene):
-	#this could be it's own module, and have a generic ending Scene
+	#this could be it's own file, with a generic ending Scene
 	def firstStart(self):
 		font.init()
 		self.gameOverText = loadText("Game Over!", font.SysFont(("comicsans"), round(Y/4)))
@@ -28,7 +28,6 @@ class GameOver(Scene):
 		)
 		self.rects = (t[0].get_rect(),t[1].get_rect(),)
 		for i in self.rects: i.center = VIEW.center+Vector(0,Y/15)
-		
 
 	def onDraw(self):
 		screen.draw_color = 20,20,20
@@ -54,17 +53,29 @@ class JumpGame(Scene):
 	def onDraw(self):
 		screen.draw_color = 20,20,20
 		screen.clear()
-		if self.player.centery < VIEW.centerx: self.momentum -= 1
-		else: self.player.centery = VIEW.centerx
+		
+		Ypos = VIEW.centerx
+		
+		if self.player.centery < Ypos: self.momentum -= 1
+		else: self.player.centery = Ypos
 		if 0 < self.t:
-			self.player.centery = y = VIEW.centerx-13*self.t+.3*self.t**2
+			self.player.centery = y = Ypos-13*self.t+.3*self.t**2
 			self.t += 1
-			if y > VIEW.centerx: self.t = 0; self.player.centery = VIEW.centerx
+			if y > Ypos:
+				self.t = 0
+				self.player.centery = Ypos
 		self.pos += 10
 		screen.draw_color = 255,255,20
 		screen.fill_rect(self.player)
 		screen.draw_color = 255,255,255
-		#screen.draw_line
+		
+		screen.logical_size = 80,50
+		#this method is really buggy, you shouldn't touch it under normal circumstances
+		#https://github.com/pygame-community/pygame-ce/issues/2923
+		#https://github.com/pygame-community/pygame-ce/issues/3244
+		#https://github.com/pygame-community/pygame-ce/issues/3245
+		screen.draw_line((0,42),(80,42))
+		screen.logical_size = SIZE
 	def keyHandler(self, ks: ScancodeWrapper) -> bool | None:
 		if ks[K_SPACE] and not self.t: self.t = 1
 	#	l,r = ks[K_LEFT]or ks[K_a],ks[K_RIGHT]or ks[K_d]
