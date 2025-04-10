7a25e74bce9c8faf218e50d5715ff5fd5fb86d10
ImNotMeAnymore <172158294+ImNotMeAnymore@users.noreply.github.com>
2025-04-10 18:03:53 -0400

    Jump is almost fineshed now

--- nengin/examples/jumper.py
+++ nengin/examples/jumper.py
@@ -1,9 +1,9 @@
 from pygame.key import ScancodeWrapper
 import nengin as ng
 from nengin import Scene, addScene, screen, Vector
-from pygame import font, FRect as Rect, K_LEFT, K_RIGHT, K_a, K_d, K_r, K_SPACE
+from pygame import font, FRect as Rect, K_r, K_SPACE
 from pygame._sdl2.video import Texture			# pyright: ignore
-from random import choice, randint
+from random import randint
 
 
 X,Y = SIZE = Vector(8,5)*120
@@ -17,11 +17,11 @@ class GameOver(Scene):
 	#this could be it's own file, with a generic ending Scene
 	def firstStart(self):
 		font.init()
-		self.gameOverText = loadText("Game Over!", font.SysFont(("comicsans"), round(Y/4)))
+		self.gameOverText = loadText("Game Over!", font.SysFont(("comicsans","ubuntu","sans"), round(Y/4)))
 		self.gameOverRect = self.gameOverText.get_rect()
 		self.gameOverRect.center = VIEW.center
 		self.gameOverRect.y -= Y/5
-		f = font.SysFont(("comicsans"), round(Y/15))
+		f = font.SysFont(("comicsans","ubuntu","sans"), round(Y/15))
 		self.texts = t = (
 			loadText("You lost!  press r to play again or esc to quit",f),
 			loadText("You won!  press r to play again or esc to quit",f),
@@ -47,42 +47,52 @@ class JumpGame(Scene):
 		self.player = p = Rect(0,0,8*4,5*10)
 		p.centery = VIEW.centerx
 		p.centerx = VIEW.centery/3*2
-		self.momentum = -9
 		self.t = 0
+		self.OBS = []
 
 	def onDraw(self):
 		screen.draw_color = 20,20,20
 		screen.clear()
-		
 		Ypos = VIEW.centerx
-		
-		if self.player.centery < Ypos: self.momentum -= 1
-		else: self.player.centery = Ypos
 		if 0 < self.t:
 			self.player.centery = y = Ypos-13*self.t+.3*self.t**2
 			self.t += 1
 			if y > Ypos:
 				self.t = 0
 				self.player.centery = Ypos
-		self.pos += 10
+		self.pos += 8+(self.pos/5000)
 		screen.draw_color = 255,255,20
 		screen.fill_rect(self.player)
-		screen.draw_color = 255,255,255
-		
-		screen.logical_size = 80,50
-		#this method is really buggy, you shouldn't touch it under normal circumstances
+		screen.draw_color = 235,235,235
+
+		screen.logical_size = 40,25
+		#logical_size is really buggy, you shouldn't touch it under normal circumstances
 		#https://github.com/pygame-community/pygame-ce/issues/2923
 		#https://github.com/pygame-community/pygame-ce/issues/3244
 		#https://github.com/pygame-community/pygame-ce/issues/3245
-		screen.draw_line((0,42),(80,42))
+		screen.draw_line((0,21),(40,21))
 		screen.logical_size = SIZE
+		
+		if not self.OBS: self.OBS.append(self.pos+SIZE.x+160)
+		elif self.OBS[-1]+300 < self.pos+SIZE.x:
+			if not randint(0,55): self.OBS.append(self.pos+SIZE.x+160)
+		
+		
+		self.OBS = [i for i in self.OBS if i > self.pos]
+		
+		screen.draw_color = 235,20,235
+		r = Rect(0,0,80,48)
+		r.centery = Ypos
+		dead = False
+		for i in self.OBS:
+			r.right = i-self.pos
+			screen.fill_rect(r)
+			if r.colliderect(self.player): dead = True
+		
+		if dead: self.changeScene("end")
+			#screen.draw_line((i-self.pos,0),(i-self.pos,SIZE.y))
+		
 	def keyHandler(self, ks: ScancodeWrapper) -> bool | None:
 		if ks[K_SPACE] and not self.t: self.t = 1
-	#	l,r = ks[K_LEFT]or ks[K_a],ks[K_RIGHT]or ks[K_d]
-	#	if l and r: return False
-	
-		
-		#if u and self.me.top > 0: self.me.top -= 4
-		#if d and self.me.bottom < Y: self.me.bottom += 4
 
 if __name__ == "__main__": ng.Game("jump")
